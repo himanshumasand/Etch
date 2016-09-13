4cca3b054282fce7c05f9fd671d515b9cffef00f
hmasand <masandh@gmail.com>
2016-09-13 07:53:12 -0700

    Code and UI cleanup

--- app/src/main/java/com/hmasand/etch/activities/CreateEtchActivity.java
+++ app/src/main/java/com/hmasand/etch/activities/CreateEtchActivity.java
@@ -1,9 +1,11 @@
 package com.hmasand.etch.activities;
 
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -15,18 +17,17 @@ import com.hmasand.etch.R;
 import com.hmasand.etch.models.RichLinkPreviewData;
 import com.squareup.picasso.Picasso;
 
-public class CreateEtchActivity extends AppCompatActivity implements View.OnClickListener, RichLinkPreviewData.CreateRichLinkPreviewListener {
+public class CreateEtchActivity extends AppCompatActivity implements RichLinkPreviewData.CreateRichLinkPreviewListener {
 
     private Button mBtEtch;
-    private Button mBtCancel;
     private LinearLayout mEtchItem;
-
-    private RichLinkPreviewData rlpData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setBackgroundDrawable(new ColorDrawable(0));
         handleIntentReceived();
     }
 
@@ -44,7 +45,7 @@ public class CreateEtchActivity extends AppCompatActivity implements View.OnClic
     }
 
     @Override
-    public void onCreateRichLinkPreviewSuccess(RichLinkPreviewData data) {
+    public void onCreateRichLinkPreviewSuccess(final RichLinkPreviewData data) {
         setContentView(R.layout.activity_create_etch);
 
         mEtchItem = (LinearLayout) findViewById(R.id.llItem);
@@ -52,31 +53,18 @@ public class CreateEtchActivity extends AppCompatActivity implements View.OnClic
         TextView tvRichLinkDesc = (TextView) mEtchItem.findViewById(R.id.tvRichLinkDescription);
         ImageView ivRichLinkThumb = (ImageView) mEtchItem.findViewById(R.id.ivRichLinkThumbnail);
 
-        rlpData = data;
         tvRichLinkTitle.setText(data.title);
         tvRichLinkDesc.setText(data.description);
         Picasso.with(this).load(data.imageUrl).resize(100, 100).centerCrop().into(ivRichLinkThumb);
 
         mBtEtch = (Button) findViewById(R.id.btEtch);
-        mBtEtch.setOnClickListener(this);
-
-        mBtCancel = (Button) findViewById(R.id.btCancel);
-        mBtCancel.setOnClickListener(this);
-    }
-
-    @Override
-    public void onClick(View view) {
-        switch (view.getId()) {
-
-            case R.id.btEtch:
+        mBtEtch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
                 DatabaseReference entriesRef = FirebaseDatabase.getInstance().getReference().child("entries");
-                entriesRef.push().setValue(rlpData);
+                entriesRef.push().setValue(data);
                 finish();
-                break;
-
-            case R.id.btCancel:
-                finish();
-                break;
-        }
+            }
+        });
     }
 }
